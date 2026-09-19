857a206fa077daab597863571d45e1ef1853bfcb
Hiraoka <pinggang84@gmail.com>
2026-09-19 17:17:48 +0900

    feat: お問い合わせ確認画面を実装

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ContactController::class, 'index'])->name('contact.index');
+Route::post('/contacts/confirm', [ContactController::class, 'confirm'])
+    ->name('contact.confirm');
