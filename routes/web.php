97a6a1ce298fdeaf2f4ec0d891fcef2f32bf3e30
Hiraoka <pinggang84@gmail.com>
2026-09-19 19:45:44 +0900

    feat: お問い合わせ登録とThanks画面を実装

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ContactController::class, 'index'])->name('contact.index');
 Route::post('/contacts/confirm', [ContactController::class, 'confirm'])
     ->name('contact.confirm');
+Route::post('/contacts', [ContactController::class, 'store'])
+    ->name('contact.store');
+
+Route::get('/thanks', function () {
+    return view('contact.thanks');
+})->name('contact.thanks');
